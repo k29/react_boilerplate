9e106d862e4f12894c8c16a7375b6ecb07d9d119
Kaustubh Nawade <nawadk@amazon.com>
2019-12-30 15:03:46 +0530

    basic react boilerplate setup from scratch. need to setup tests and folders

---
+++ src/components/BadPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Since this component is simple and static, there's no parent container for it.
+const BadPage = () => {
+  return (
+    <div>
+      <h2 className="alt-header">404 page</h2>
+    </div>
+  );
+};
+
+export default BadPage;
