39516f444962a343ca882696e73734aa4a0038c9
Kaustubh Nawade <nawadk@amazon.com>
2019-12-30 17:38:49 +0530

    some other basic setup done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div>
-      <h1>React Slingshot</h1>
+      <h1>React Boilerplate</h1>
 
       <h2>Get Started</h2>
       <ol>
         <li>Review the <Link to="/about">about app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
       </ol>
     </div>
   );
